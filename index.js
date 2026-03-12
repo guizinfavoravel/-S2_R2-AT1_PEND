d3d27a380909e264c4dc3cf5a0eb2b0e5b359efb
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-03-12 09:50:38 -0300

    atividade 4

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 function adicionarItem() {
 
-            var input = document.getElementById("itemInput");
-            var lista = document.getElementById("minhaLista");
-}if (input.value.trim() !== "") {
-                var novoItem = document.createElement("li");
-                novoItem.innerText = input.value;
-                lista.appendChild(novoItem);
-                input.value = "";
-                input.focus();
-                
-            } else {
-                alert("Por favor, digite algo antes de adicionar!");
-            }
+    var input = document.getElementById("itemInput");
+    var lista = document.getElementById("minhaLista");
+    if (input.value.trim() !== "") {
+        var novoItem = document.createElement("li");
+        novoItem.innerText = input.value;
+        lista.appendChild(novoItem);
+        input.value = "";
+        input.focus();
+
+    } else {
+        alert("Por favor, digite algo antes de adicionar!");
+    }
+}
