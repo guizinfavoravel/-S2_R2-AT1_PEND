1b016dbaa694a48d6b02988e71698830b0374924
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-03-19 08:21:03 -0300

    at1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
  function enviar() {
-            var nome = document.getElementById("campoNome").value;
+            let nome = document.getElementById("campoNome").value;
             document.getElementById("mensagem").innerText = `E um prazer ${nome}`;
         }
