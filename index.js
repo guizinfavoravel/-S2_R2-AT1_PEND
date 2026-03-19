6a32e09da836af4d81d1f2896b5ccbf738ee9a78
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-03-19 08:22:08 -0300

    at2

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function mudarTexto() {
             
-            var elemento = document.getElementById("textoPrincipal");
+            let elemento = document.getElementById("textoPrincipal");
             
             elemento.innerText = "Voce foi Mogado!";
 
