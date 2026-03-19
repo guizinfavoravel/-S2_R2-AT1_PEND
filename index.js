085205a8ccf7b8b633a6cea2db6f05aca122cb89
GUILHERME MEDEIROS LEMOS <guilherme.lemos13@senaisp.edu.br>
2026-03-19 08:24:47 -0300

    at3

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 function adicionarItem() {
 
-    var input = document.getElementById("itemInput");
-    var lista = document.getElementById("minhaLista");
+    let input = document.getElementById("itemInput");
+
+    let lista = document.getElementById("minhaLista");
+    
     if (input.value.trim() !== "") {
-        var novoItem = document.createElement("li");
+        let novoItem = document.createElement("li");
         novoItem.innerText = input.value;
         lista.appendChild(novoItem);
         input.value = "";
-        input.focus();
 
     } else {
         alert("Por favor, digite algo antes de adicionar!");
